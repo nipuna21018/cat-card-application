04b23968fcc6c26750899abf8e33d3419a9346e3
Nipuna Harshana <nipuna.harshana@syscolabs.com>
2023-06-13 22:39:19 +0530

    use promises in api call

--- src/modules/cat/services/cat-image.service.ts
+++ src/modules/cat/services/cat-image.service.ts
@@ -4,11 +4,11 @@ import { ApiService } from "../../common/services/api.service";
 
 export default class CatImageService {
 
-  private apiService:any;
+  private apiService: ApiService;
   private path = '/cat/says/';
 
   constructor() {
-    const catAsAServiceUrl:any = config.catAsAServiceUrl;
+    const catAsAServiceUrl: string = config.catAsAServiceUrl || '';
     this.apiService = new ApiService(catAsAServiceUrl);
   }
 
@@ -21,18 +21,20 @@ export default class CatImageService {
    * @param size size of cat
    * @returns image buffer
    */
-  async getCatImageWithText(text:string, width:number, height:number, color:string, size:number) {
+  getCatImageWithText(text: string, width: number, height: number, color: string, size: number): Promise<Buffer> {
     const endpoint = `${this.path}${encodeURIComponent(text)}`;
     const params = { width, height, color, s: size };
 
-    try {
-      // important to make the header response type as `arraybuffer`
-      const response = await  this.apiService.makeRequest(endpoint, { params, responseType: 'arraybuffer' });
-      console.log('Cat image fetching completed successfully!');
-      return Buffer.from(response, 'binary');
-    } catch (error:any) {
-      throw new Error(`Failed to fetch cat image with text: ${error.message}`);
-    }
+    return new Promise<Buffer>((resolve, reject) => {
+      this.apiService
+        .makeRequest(endpoint, { params, responseType: 'arraybuffer' })
+        .then((response: ArrayBuffer) => {
+          console.log('Cat image fetching completed successfully!');
+          resolve(Buffer.from(response));
+        })
+        .catch((error: Error) => {
+          reject(new Error(`Failed to fetch cat image with text: ${error.message}`));
+        });
+    });
   }
-
 }
