f80b821cf63bb8298d1487f86f8849bdadb77b00
Nipuna Harshana <nipuna.harshana@syscolabs.com>
2023-06-13 22:46:22 +0530

    use promisify instead of promise object in api service

--- src/modules/common/services/api.service.ts
+++ src/modules/common/services/api.service.ts
@@ -1,25 +1,20 @@
 import axios from 'axios';
 import { AxiosInstance } from 'axios';
-
+import { promisify } from 'util';
 export class ApiService {
-
   api: AxiosInstance;
 
-  constructor(baseURL:string) {
+  constructor(baseURL: string) {
     this.api = axios.create({ baseURL });
   }
 
   makeRequest(url: string, options = {}): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this.api
-        .request({ url, ...options })
-        .then((response: any) => {
-          resolve(response.data);
-        })
-        .catch((error: Error) => {
-          reject(new Error(`Error making request to ${url}: ${error.message}`));
-        });
-    });
-  }
+    const promisifiedRequest = promisify(this.api.request).bind(this.api);
 
+    return promisifiedRequest({ url, ...options })
+      .then((response: any) => response.data)
+      .catch((error: Error) => {
+        throw new Error(`Error making request to ${url}: ${error.message}`);
+      });
+  }
 }
