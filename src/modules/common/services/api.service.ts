7c0f38c8901ee5ab0b07b7839ebf81e7cc818401
Nipuna Harshana <nipuna.harshana@syscolabs.com>
2023-06-15 16:46:39 +0530

    image not fetching issue fixed

--- src/modules/common/services/api.service.ts
+++ src/modules/common/services/api.service.ts
@@ -1,20 +1,23 @@
 import axios from 'axios';
 import { AxiosInstance } from 'axios';
-import { promisify } from 'util';
 export class ApiService {
+
   api: AxiosInstance;
 
-  constructor(baseURL: string) {
+  constructor(baseURL:string) {
     this.api = axios.create({ baseURL });
   }
 
   makeRequest(url: string, options = {}): Promise<any> {
-    const promisifiedRequest = promisify(this.api.request).bind(this.api);
-
-    return promisifiedRequest({ url, ...options })
-      .then((response: any) => response.data)
-      .catch((error: Error) => {
-        throw new Error(`Error making request to ${url}: ${error.message}`);
-      });
+    return new Promise((resolve, reject) => {
+      this.api
+        .request({ url, ...options })
+        .then((response: any) => {
+          resolve(response.data);
+        })
+        .catch((error: Error) => {
+          reject(new Error(`Error making request to ${url}: ${error.message}`));
+        });
+    });
   }
 }
